68b880ad571aa59d78b2d10e585f34aad6729f8f
tate1010 <tatecheng1010@gmail.com>
2016-08-08 17:48:35 -0400

    HomePage Layout

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import autobind from 'autobind-decorator';
 import { Link } from 'react-router'
+import {Card,CardTitle,CardText} from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import {List, ListItem} from 'material-ui/List';
 import map from '../map';
 
 class Home extends React.Component {
@@ -9,11 +12,19 @@ class Home extends React.Component {
     super(props);
     console.log(this.props);
     this.state = {menuOpen: false};
-  } 
+  }
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <h1>Party Web Service</h1>
+
+        <p> Click on the menu to perform an action </p>
+        <Card>
+            <CardTitle title="Patch Note" />
+            <CardText style={{paddingTop: 0}}>
+            </CardText>
+        </Card>
+
       </div>
     );
   }
--- src/views/Join.js
+++ src/views/Join.js
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import autobind from 'autobind-decorator';
 import { Link } from 'react-router'
-
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 export default class Join extends React.Component {
   constructor(props) {
     super(props);
     this.state = {menuOpen: false};
-  } 
+  }
   render() {
     return (
-      <div>
-        <h1>Join</h1>
-      </div>
+      <Card>
+          <CardTitle title="Join a game" />
+          <CardText style={{paddingTop: 0}}>
+
+          </CardText>
+      </Card>
     );
   }
 }
-
