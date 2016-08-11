999dd89bb08289f18ea8df8d2afae8c7a576314d
tate1010 <tatecheng1010@gmail.com>
2016-08-11 02:00:16 -0400

    /game view update

--- src/views/Game.js
+++ src/views/Game.js
@@ -6,6 +6,14 @@ import {Card,CardTitle,CardText} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import {List, ListItem} from 'material-ui/List';
 import map from '../map';
+import Chip from 'material-ui/Chip';
+import RaisedButton  from 'material-ui/RaisedButton';
+
+
+function removeplayer(){
+
+
+}
 
 class Game extends React.Component {
   constructor(props) {
@@ -13,6 +21,10 @@ class Game extends React.Component {
     console.log(this.props);
     this.state = {menuOpen: false};
   }
+startgame(){
+
+}
+
   render() {
     return (
       <div>
@@ -23,9 +35,28 @@ class Game extends React.Component {
         <CardTitle title={this.props.games.name}/>
         <CardTitle subtitle = {"Room ID:  " + this.props.games.session}/>
 
-        </Card>
 
+       <List>
+       <ListItem
+              primaryText="player1"
+            />
+
+
+       <ListItem
+              primaryText="player2"
+            />
+
+
+       <ListItem
+              primaryText="player3"
+            />
+
+       </List>
+        <RaisedButton  label="Start Game " primary={true} onClick={this.startgame} />
+      </Card>
       </div>
+
+
     );
   }
 }
