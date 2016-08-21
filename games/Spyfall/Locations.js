b19f5890c5a5ccab992f85211abb36766d7beed8
tate1010 <tatecheng1010@gmail.com>
2016-08-21 00:20:22 -0400

    Location list update

--- games/Spyfall/Locations.js
+++ games/Spyfall/Locations.js
@@ -1,11 +1,27 @@
 export default [
     {
         name: "Airport",
-        roles: ["Pilot","Janitor","Business Man","Passenger","Co Pilot","Airport Engineer","Ambassador"]
+        roles: ["Pilot","Janitor","Business Man","Passenger","Co Pilot","Airport Engineer","Ambassador","security guard","Customer Service Agent"]
     },
     {
         name: "Bank",
-        roles: ["Teller","Manager","Business Man","Robber","security guard"]
+        roles: ["Teller","Manager","Business Man","Robber","security guard","Assistant Financial Advisor","Janitor","Customer Service Agent","ATM technician"]
     },
+    {
+        name: "Movie Studio",
+        roles: ["Producer","Director","Main actor","Body double","temporary actor","special effect agent","makeup specialist","costume designer","camera man","Sound Engineer","StuntMan"]
+    },
+    {
+        name: "Space Station",
+        roles: ["Alien","Scientist","Main actor","astronaut","Engineer","Rocket specialist ","Commander","Tourist","Doctor"]
+    },
+    {
+        name: "Pirate Ship",
+        roles: ["Navy Army","Navigator","Captain","Sniper","Cook","Musician ","recruit","Captive","Doctor"]
+    },
+    {
+        name: "School",
+        roles: ["Janitor","Lunch Lady","Gym Teacher","Student","Principal","librarian ","Vice principal","Science Teacher","Math Teacher"]
+
 
 ];
