b9c0e7018b8375ff3c975ef741f81aa45cb8d154
tate1010 <tatecheng1010@gmail.com>
2016-08-20 23:55:27 -0400

    location list

--- games/Mafia/Mafia.js
+++ games/Mafia/Mafia.js
@@ -5,7 +5,73 @@ export default class Mafia{
     get description(){
         return 'In Mafia, there are "Mafia" members who kill people every night. Investigators try to figure out who is killing everyone. Then, at sunset, the collective votes to kill the suspects.'
     }
-    constructor(){
+    createGame(session){ //{gameData, }
+        this.games[session.id] = {
+            roles: {
 
+
+
+            },
+            admin: "",
+            started: false,
+            location: -1,
+
+        };
+        this.actions = {
+            START: 'start_game'
+        };
+    }
+    destroyGame(session){
+        delete this.games[session.id];
+    }
+    addUser(session, userId){
+        if(this.games[session.id].roles[userId]) return; //don't make existing users blank
+        if(Object.keys(this.games[session.id].roles).length == 0){
+            this.games[session.id].admin = userId;
+        }
+        this.games[session.id].roles[userId] = "";
+    }
+    removeUser(session, user){
+        delete this.games[session.id].roles[user];
+    }
+    receiveMessage(session, userId, action){
+        var game = this.games[session.id];
+        switch(action){
+            case this.actions.START:  //START GAME MESSAGE RECEIVED
+                game.started = true;
+                game.location = Math.floor(Math.random()*Locations.length);
+
+                game.roles[userId] = "Janitor";
+
+                break;
+        }
+    }
+    getUserMessages(session, userId){
+
+        if(this.games[session.id].started){
+
+           if(this.games[session.id].roles[userId]="Spy"){
+
+
+             return [{text:"Location: ???"
+
+               , actions: []}];
+
+           }
+             else{
+              return [{text: Locations[this.games[session.id].location].name
+
+                , actions: []}];
+              }
+
+        }else{
+            var waiting = [{text: "Waiting for players to join...", actions: []}];
+            if(this.games[session.id].admin == userId) waiting.push({text: "You are the game admin. Press 'Start Game' when you want to begin.", actions: [{id: this.actions.START, text: "Start Game"}]});
+            return waiting;
+        }
+    }
+    constructor(engine){
+        this.engine = engine;
+        this.games = {};
     }
 }
--- games/Spyfall/Locations.js
+++ games/Spyfall/Locations.js
@@ -2,5 +2,9 @@ export default [
     {
         name: "Airport",
         roles: ["Pilot","Janitor","Business Man","Passenger","Co Pilot","Airport Engineer","Ambassador"]
-    }
+    },
+    {
+        name: "Bank",
+    },
+
 ];
